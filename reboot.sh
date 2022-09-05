88fefc46155ed4b12129a2427cea3c6d806deced
Pliny <pliny@gmail.com>
2022-09-05 09:05:30 -0700

    Add reboot

---
+++ reboot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+device=$1
+IP=$(cat DeviceNameIP.txt | grep -w $device | cut -d '=' -f1)
+adb connect "$IP"
+adb -s "$IP" shell "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas"
+sleep 5
+adb -s "$IP" shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
