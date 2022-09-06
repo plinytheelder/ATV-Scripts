977c01526ba965e46882698266587c6bdce5b724
plinytheelder <jwjohnson310@gmail.com>
2022-09-06 14:08:29 -0700

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -4,7 +4,7 @@ device=$1
 echo "$device"
 echo "[$device] Connecting"
 ./adb connect "$device"
-./adb -s "$device" push hostname.sh /data/local/tmp/
+./adb -s "$device" push other/hostname.sh /data/local/tmp/
 ./adb -s "$device" shell "su -c 'mv /data/local/tmp/hostname.sh /data/adb/service.d/'"
 ./adb -s "$device" shell "su -c 'chmod +x /data/adb/service.d/hostname.sh'"
 ./adb -s "$device" reboot
