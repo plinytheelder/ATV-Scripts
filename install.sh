740bd6747498e125727b2ff514d3f8e47ef61cd3
plinytheelder <jwjohnson310@gmail.com>
2022-10-04 13:37:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-while IFS== read device name;do
-    adb connect $device
-    type=$(adb -s $device shell uname -m)
+device=$1
+    ./adb connect $device
+    type=$(./adb -s $device shell uname -m)
         if [ "$type" = "aarch64" ]; then
-                adb -s $i install -r -d ./apks/atlas.apk
-                adb -s $i install -r -d ./apks/pogo64.apk
-                adb -s $i shell 'am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService'
-                echo "$name is done"
+                ./adb -s $device install -r -d ./apks/downloads/pogo64.apk
+                ./adb -s $device shell 'am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService'
+                echo "$device is done"
         else
-                adb -s $i install -r -d ./apks/atlas.apk
-                adb -s $i install -r -d ./apks/pogo32.apk
-                adb -s $i shell 'am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService'
-                echo "$name is done"
+                ./adb -s $device install -r -d ./apks/downloads/pogo32.apk
+                ./adb -s $device shell 'am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService'
+                echo "$device is done"
         fi
-done <"DeviceNameIP.txt"
