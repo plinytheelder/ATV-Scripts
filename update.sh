43112852c3288104672d5c8cd6b930fea3c80932
plinytheelder <jwjohnson310@gmail.com>
2023-04-21 09:47:25 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 device=$1
+version=$2
     ./adb connect $device
     type=$(./adb -s $device shell uname -m)
-        if [ "$type" = "aarch64" ]; then
+    versioninfo=$(./adb -s $device shell dumpsys package com.nianticlabs.pokemongo | grep versionName | head -n1 | sed 's/ *versionName=//')
+        if [ "$version" = "$versioninfo" ]; then
+                ./adb disconnect $device
+        elif [ "$type" = "aarch64" ]; then
                 ./adb -s $device install -r -d ./apks/downloads/pogo64.apk
                 ./adb -s $device shell 'am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService'
                 echo "$device is done"
