75621e9010f4990ec61e0f68029a62a23a2e73ac
plinytheelder <jwjohnson310@gmail.com>
2023-04-21 09:49:23 -0700

    Update updateall.sh

--- updateall.sh
+++ updateall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+versioninput=$1
 readarray -d ' ' devices < <(cat DeviceNameIP.txt | cut -f1 -d'=' | tail -n +2 | xargs)
 for device in "${devices[@]}"; do
-    ./update.sh $device
+    ./update.sh $device $versioninput
 done
