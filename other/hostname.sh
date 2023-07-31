6b7730aed073907a0cccf6ab3f37b1ee9df628b4
Chris R <chrisrnz@users.noreply.github.com>
2023-07-31 22:58:29 +1200

    Update hostname.sh
    Update hostname.sh to work with either Atlas or GC

--- other/hostname.sh
+++ other/hostname.sh
@@ -1,4 +1,9 @@
 #!/system/bin/sh
 sleep 10
-deviceName=$(cat /data/local/tmp/atlas_config.json | tr , '\n' | grep -w 'deviceName' | awk -F ":" '{ print $2 }' | tr -d \"})
+if [[ -f /data/local/tmp/atlas_config.json ]] ;then
+    deviceName=$(cat /data/local/tmp/atlas_config.json | tr , '\n' | egrep -w 'deviceName' | awk -F ":" '{ print $2 }' | tr -d \"})
+elif [[ -f /data/local/tmp/config.json ]] ;then
+    deviceName=$(cat /data/local/tmp/config.json | tr , '\n' | egrep -w 'device_name' | awk -F ":" '{ print $2 }' | tr -d \"})
+else exit 0
+fi
 su -c setprop net.hostname $deviceName
