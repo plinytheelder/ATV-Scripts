7beac846f96fc6637b0a558f34add790f454d457
plinytheelder <jwjohnson310@gmail.com>
2023-12-05 20:09:52 -0800

    Update ATVdetailsSender.sh

--- emagisk/custom/ATVdetailsSender.sh
+++ emagisk/custom/ATVdetailsSender.sh
@@ -6,8 +6,7 @@ logfile='/data/local/tmp/emagisk.log'
 reboottype=$1
 
 #Configs
-atlas_conf="/data/local/tmp/atlas_config.json"
-atlas_log="/data/local/tmp/atlas.log"
+mitm_conf="/data/local/tmp/config.json"
 
 source $CONFIGFILE
 export useSender atvdetails_interval atvdetails_receiver_host atvdetails_receiver_port
@@ -23,27 +22,23 @@ while true
 
 # generic
     RPL=$(($atvdetails_interval/60))
-    deviceName=$(cat $atlas_conf | tr , '\n' | grep -w 'deviceName' | awk -F ":" '{ print $2 }' | tr -d \"})
+    deviceName=$(cat $mitm_conf | tr , '\n' | grep -w 'device_name' | awk -F ":" '{ print $2 }' | tr -d \"})
     arch=$(uname -m)
     productmodel=$(getprop ro.product.model)
     pogo=$(dumpsys package com.nianticlabs.pokemongo | grep versionName | head -n1 | sed 's/ *versionName=//')
-    atlas=$(dumpsys package com.pokemod.atlas | grep versionName | head -n1 | sed 's/ *versionName=//')
+    mitmversion=$(dumpsys package com.gocheats.launcher | grep versionName | head -n1 | sed 's/ *versionName=//')
     temperature=$(cat /sys/class/thermal/thermal_zone0/temp | cut -c -2)
     magisk=$(magisk -c | sed 's/:.*//')
-    macw=$([ -d /sys/class/net/wlan0 ] && ifconfig wlan0 |grep 'HWaddr' |awk '{ print ($NF) }' || echo 'na')
     mace=$(ifconfig eth0 |grep 'HWaddr' |awk '{ print ($NF) }')
     ip=$(ifconfig wlan0 |grep 'inet addr' |cut -d ':' -f2 |cut -d ' ' -f1 && ifconfig eth0 |grep 'inet addr' |cut -d ':' -f2 |cut -d ' ' -f1)
-    ext_ip=$(curl -k -s https://ifconfig.me/)
     hostname=$(getprop net.hostname)
     playstore=$(dumpsys package com.android.vending | grep versionName | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
     proxyinfo=$(proxy=$(settings list global | grep "http_proxy=" | awk -F= '{ print $NF }'); [ -z "$proxy" ] || [ "$proxy" = ":0" ] && echo "none" || echo "$proxy")
 # atv performance
-# atlas config
-    authBearer=$(cat $atlas_conf | tr , '\n' | grep -w 'authBearer' | awk -F ":" '{ print $2 }' | tr -d \"})
-    token=$(cat $atlas_conf | tr , '\n' | grep -w 'deviceAuthToken' | awk -F ":" '{ print $2 }' | tr -d \"})
-    email=$(cat $atlas_conf | tr , '\n' | grep -w 'email' | awk -F ":" '{ print $2 }' | tr -d \"})
-    rdmUrl=$(cat $atlas_conf | tr , '\n' | grep -w 'rdmUrl' | awk -F "\"" '{ print $4 }')
-    onBoot=$(cat $atlas_conf | tr , '\n' | grep -w 'runOnBoot' | awk -F ":" '{ print $2 }' | tr -d \"})
+# config
+    token=$(cat $mitm_conf | tr , '\n' | grep -w 'api_key' | awk -F ":" '{ print $2 }' | tr -d \"})
+    workers=$(cat $mitm_conf | tr , '\n' | grep -w 'workers_count' | awk -F ":" '{ print $2 }' | tr -d \"})
+    rotomUrl=$(cat $mitm_conf | tr , '\n' | grep -w 'rotom_url' | awk -F "\"" '{ print $4 }')
 
 #send data
     curl -k -X POST $atvdetails_receiver_host:$atvdetails_receiver_port/webhook -H "Accept: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
@@ -53,22 +48,18 @@ while true
     "arch": "${arch}",
     "productmodel": "${productmodel}",
     "pogo": "${pogo}",
-    "atlas": "${atlas}",
+    "mitmversion": "${mitmversion}",
     "temperature": "${temperature}",
     "magisk": "${magisk}",
-    "macw": "${macw}",
     "mace": "${mace}",
     "ip": "${ip}",
-    "ext_ip": "${ext_ip}",
     "hostname": "${hostname}",
     "playstore": "${playstore}",
     "proxyinfo": "${proxyinfo}",
     "reboot": "${reboottype}",
-    "authBearer": "${authBearer}",
     "token": "${token}",
-    "email": "${email}",
-    "rdmUrl": "${rdmUrl}",
-    "onBoot": "${onBoot}"
+    "workers": "${workers}",
+    "rotomUrl": "${rotomUrl}"
 }
 
 DATA
