8edcb616a948895ee76b678834a7ed8fd12cf503
plinytheelder <jwjohnson310@gmail.com>
2023-12-09 05:35:21 -0800

    Delete updateall.sh

--- updateall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-versioninput=$1
-readarray -d ' ' devices < <(cat DeviceNameIP.txt | cut -f1 -d'=' | tail -n +2 | xargs)
-for device in "${devices[@]}"; do
-    ./update.sh $device $versioninput
-done
