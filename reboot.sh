b8a04cf4fee7e9d97eaaa5f0e23db68861f1a54a
plinytheelder <jwjohnson310@gmail.com>
2023-12-09 05:37:15 -0800

    Delete reboot.sh

--- reboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-device=$1
-IP=$(cat DeviceNameIP.txt | grep -w $device | cut -d '=' -f1)
-adb connect "$IP"
-adb -s "$IP" shell "su -c am force-stop com.nianticlabs.pokemongo & am force-stop com.pokemod.atlas"
-adb -s "$IP" shell "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
