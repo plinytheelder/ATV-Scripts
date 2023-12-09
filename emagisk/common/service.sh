dd92ee6e9a9744d4b00434b9551ba390da911702
plinytheelder <jwjohnson310@gmail.com>
2023-12-08 16:17:40 -0800

    Update service.sh

--- emagisk/common/service.sh
+++ emagisk/common/service.sh
@@ -36,6 +36,10 @@ if [ -f "$MODDIR/ATVServices.sh" ]; then
     sleep 20
     log "Starting ATVServices.sh"
     . "$MODDIR/ATVServices.sh"
+fi
+
+if [ -f "$MODDIR/ATVdetailsSender.sh" ]; then
+    sleep 20
     log "Starting ATVdetailsSender.sh"
     . "$MODDIR/ATVdetailsSender.sh"
 fi
