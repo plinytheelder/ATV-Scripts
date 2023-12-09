ef4aef38a91528ea10f48b7ea016a22298bee6bc
plinytheelder <jwjohnson310@gmail.com>
2023-12-09 05:34:48 -0800

    Delete other directory

--- other/hostname.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-sleep 10
-deviceName=$(cat /data/local/tmp/atlas_config.json | tr , '\n' | grep -w 'deviceName' | awk -F ":" '{ print $2 }' | tr -d \"})
-su -c setprop net.hostname $deviceName
