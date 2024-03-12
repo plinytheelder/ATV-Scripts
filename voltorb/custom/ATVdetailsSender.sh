6e0128f0ebc9a8ddca188159b03feb8d8a10a6ec
plinytheelder <jwjohnson310@gmail.com>
2024-03-12 10:38:48 -0700

    Update ATVdetailsSender.sh

--- voltorb/custom/ATVdetailsSender.sh
+++ voltorb/custom/ATVdetailsSender.sh
@@ -16,7 +16,6 @@ fi
 
 # generic
     RPL=$(($atvdetails_interval/60))
-    deviceName=$(su -c cat $mitm_conf | grep "origin" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' || echo 'na')
     arch=$(uname -m)
     productmodel=$(getprop ro.product.model)
     voltorbversion=$(cat "$MODDIR/version_lock" || echo 'na')
@@ -29,16 +28,11 @@ fi
     hostname=$(getprop net.hostname)
     playstore=$(dumpsys package com.android.vending | grep versionName | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
     proxyinfo=$(proxy=$(settings list global | grep "http_proxy=" | awk -F= '{ print $NF }'); [ -z "$proxy" ] || [ "$proxy" = ":0" ] && echo "none" || echo "$proxy")
-# atv performance
-# config
-    workers=$(su -c cat $mitm_conf | grep "workers" | awk -F "\"" '{print tolower($4)}' || echo 'na')
-    rotomUrl=$(su -c cat $mitm_conf | grep "websocketurl" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' || echo 'na')
 
 #send data
     curl -k -X POST $atvdetails_receiver_host:$atvdetails_receiver_port/webhook -H "Accept: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
 {
     "RPL": "${RPL}",
-    "deviceName": "${deviceName}",
     "arch": "${arch}",
     "productmodel": "${productmodel}",
     "voltorbversion": "${voltorbversion}",
@@ -51,8 +45,6 @@ fi
     "hostname": "${hostname}",
     "playstore": "${playstore}",
     "proxyinfo": "${proxyinfo}",
-    "workers": "${workers}",
-    "rotomUrl": "${rotomUrl}",
 }
 
 DATA
