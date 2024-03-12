bbdf0026becd3a05b5013a6c331eacad99a51d16
plinytheelder <jwjohnson310@gmail.com>
2024-03-12 09:29:15 -0700

    Update ATVdetailsSender.sh

--- voltorb/custom/ATVdetailsSender.sh
+++ voltorb/custom/ATVdetailsSender.sh
@@ -16,10 +16,10 @@ fi
 
 # generic
     RPL=$(($atvdetails_interval/60))
-    deviceName=$(su -c cat $mitm_conf | grep "origin" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
+    deviceName=$(su -c cat $mitm_conf | grep "origin" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' || echo 'na')
     arch=$(uname -m)
     productmodel=$(getprop ro.product.model)
-    voltorbversion=$(cat "$MODDIR/version_lock")
+    voltorbversion=$(cat "$MODDIR/version_lock" || echo 'na')
     pogo=$(dumpsys package com.nianticlabs.pokemongo | grep versionName | head -n1 | sed 's/ *versionName=//')
     mitmversion=$(dumpsys package de.vahrmap.vmapper | grep versionName | head -n1 | sed 's/ *versionName=//')
     temperature=$(cat /sys/class/thermal/thermal_zone0/temp | cut -c -2)
@@ -31,8 +31,8 @@ fi
     proxyinfo=$(proxy=$(settings list global | grep "http_proxy=" | awk -F= '{ print $NF }'); [ -z "$proxy" ] || [ "$proxy" = ":0" ] && echo "none" || echo "$proxy")
 # atv performance
 # config
-    workers=$(su -c cat $mitm_conf | grep "workers" | awk -F "\"" '{print tolower($4)}')
-    rotomUrl=$(su -c cat $mitm_conf | grep "websocketurl" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }')
+    workers=$(su -c cat $mitm_conf | grep "workers" | awk -F "\"" '{print tolower($4)}' || echo 'na')
+    rotomUrl=$(su -c cat $mitm_conf | grep "websocketurl" | awk -F">" '{ print $2 }' | awk -F"<" '{ print $1 }' || echo 'na')
 
 #send data
     curl -k -X POST $atvdetails_receiver_host:$atvdetails_receiver_port/webhook -H "Accept: application/json" -H "Content-Type: application/json" --data-binary @- <<DATA
